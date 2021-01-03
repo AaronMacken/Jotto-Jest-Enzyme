3d6d2aa243a8e70b2ea7405663e993e0e00c162b
Aaron Macken <aaron.a.macken@gmail.com>
2021-01-03 11:00:41 -0600

    Create input.js component with tests & mock fns

--- src/Input.js
+++ src/Input.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Input = () => {
-    return (
-        <div className="input">
-            
-        </div>
-    )
-}
+const Input = ({ secretWord }) => {
+  // since we are going to mock this function, we are not able to destruct useState from react
+  const [currentGuess, setCurrentGuess] = React.useState("");
 
-export default Input
+  // TODO: update guessedWords
+  // TODO: check against secretWord and update
+
+  return (
+    <div className="input">
+      <form className="form-inline">
+        <input
+          className="inputBox mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button
+          className="submitButton btn btn-primary mb-2"
+          onClick={(e) => {
+            e.preventDefault();
+            setCurrentGuess("");
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
+};
+
+export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import checkPropTypes from 'check-prop-types';
 import { shallow } from 'enzyme';
 import Input from './Input';
 
+
+const defaultProps = { secretWord: 'Party' };
+
 const setup = () => {
-    return shallow(<Input />);
+    return shallow(<Input {...defaultProps} />);
 };
 
 describe('Input component', () => {
@@ -12,5 +16,42 @@ describe('Input component', () => {
         const inputWrapper = input.find('.input');
 
         expect(inputWrapper.length).toBe(1);
-    })
+    });
+
+    it('should have a `secretWord` prop', () => {
+        const propError = checkPropTypes(Input.propTypes, defaultProps, 'prop', Input.secretWord);
+
+        expect(propError).toBe(undefined);
+    });
 });
+
+describe('state controlled input field', () => {
+    let inputComponent;
+    let mockSetCurrentGuess = jest.fn();
+
+    beforeEach(() => {
+        mockSetCurrentGuess.mockClear();
+        React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+        inputComponent = setup();
+    });
+
+    it('should update state with value of input box on change', () => {
+        const inputBox = inputComponent.find('.inputBox');
+
+        // mock data, as if this were typed into the input element
+        const mockEvent = { target: { value: 'train'}};
+        // use the simulate function to call click & pass in the mock data
+        inputBox.simulate('change', mockEvent);
+
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+    });
+
+    it('should set the input box to an empty string once submitted', () => {
+        const submitButton = inputComponent.find('.submitButton');
+
+        const mockEvent = { preventDefault(){}}
+
+        submitButton.simulate('click', mockEvent);
+        expect (mockSetCurrentGuess).toHaveBeenCalledWith('');
+    });
+}); 
